c061c5c5c82582f868d6b0b7a0d6c2f864053c26
Silvao <silvio.gabrielgt2000@hotmail.com>
2023-07-18 15:16:18 -0300

    configured posthog and sentry var

--- apps/builder/src/features/dashboard/queries/createTypebotQuery.ts
+++ apps/builder/src/features/dashboard/queries/createTypebotQuery.ts
@@ -7,7 +7,7 @@ export const createTypebotQuery = async ({
 }: Pick<Typebot, 'folderId' | 'workspaceId'>) => {
   const typebot = {
     folderId,
-    name: 'My typebot',
+    name: 'Meu Hacklead',
     workspaceId,
   }
   return sendRequest<Typebot>({
