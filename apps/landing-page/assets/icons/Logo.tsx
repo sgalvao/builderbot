54c9d5ad31ba011add1c2da45624f04e295bff60
Silvao <silvio.gabrielgt2000@hotmail.com>
2023-07-18 14:19:26 -0300

    changed to correct logo

--- apps/landing-page/assets/icons/Logo.tsx
+++ apps/landing-page/assets/icons/Logo.tsx
@@ -3,5 +3,5 @@ import React from 'react'
 import Image from 'next/image'
 
 export const Logo = () => (
-  <Image src={'/logo.png'} width={90} height={90} alt="hack leads logo" />
+  <Image src={'/logobranca.png'} width={90} height={90} alt="hack leads logo" />
 )
